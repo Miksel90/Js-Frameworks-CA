9964f7e9057b767aeaecc07f8b9cfa3838ed8ef4
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2024-03-08 15:40:39 +0100

    cart icon upodates according to items in the cart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,8 +1,15 @@
 import styles from "./CartIcon.module.css";
 import { Link } from "react-router-dom";
 import { FaCartShopping } from "react-icons/fa6";
+import useStore from "../../store/CartStore";
 
-const CartIcon = ({ itemCount }) => {
+const CartIcon = () => {
+  const cart = useStore((state) => state.cart);
+  const itemCount = cart.reduce(
+    (count, product) => count + (product.quantity || 1),
+    0
+  );
+
   return (
     <Link to={`/cart`}>
       <div className={styles.cartIcon}>
